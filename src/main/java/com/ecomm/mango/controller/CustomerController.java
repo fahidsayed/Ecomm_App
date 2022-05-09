3e60986986e4408f8d093617a5a6e75e00e42644
fahid <fahid.sayed@gmail.com>
2022-05-09 17:28:30 +0530

    Customer Edit and get All Customers added

--- src/main/java/com/ecomm/mango/controller/CustomerController.java
+++ src/main/java/com/ecomm/mango/controller/CustomerController.java
@@ -1,8 +1,14 @@
 package com.ecomm.mango.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ecomm.mango.dto.CustomerDTO;
@@ -17,5 +23,20 @@ public class CustomerController {
     @PostMapping("/addCustomer")
     public String addCustomer(@RequestBody CustomerDTO customerdto) {
     	return customerService.addCustomer(customerdto);
+    }
+    
+    @GetMapping("/getAllCustomers")
+    public List<CustomerDTO> getAllCustomers() {
+    	return customerService.getAllCustomers();
+    }
+    
+    @GetMapping("/getCustomer")
+    public CustomerDTO getCustomer(@RequestParam Integer customerId) {
+    	return customerService.getCustomer(customerId);
+    }
+    
+    @PutMapping("/editCustomer")
+    public CustomerDTO editCustomer(@RequestBody CustomerDTO customerdto) {
+    	return customerService.editCustomerDetails(customerdto);
     }
 }
